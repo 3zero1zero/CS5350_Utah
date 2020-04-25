387481d18ca6c59590971b06a8267944d82011c2
x1060544 <35540647+3zero1zero@users.noreply.github.com>
2020-04-25 16:11:29 -0600

    Add files via upload

---
+++ Neural_Network/neuralNet.py
@@ -0,0 +1,189 @@
+from random import random
+from math import exp
+import numpy as np
+import random as rand
+
+def forward_pass(network, row):
+    if (row is not network):
+        inputs = row
+    else:
+        print ("forward_pass has the wrong input")
+    for layer in network:
+        new_inputs = []
+        blade = 1
+        while (blade > 0):
+            for neuron in layer:
+                act = neuron['w'][-1]
+                if (blade < 2):
+                    lengtht = len(neuron['w'])
+                    if (lengtht > 0):
+                        for i in range(lengtht - 1):
+                            lengtht = lengtht + 1
+                            act += neuron['w'][i] * inputs[i]
+                            acting = exp(-act)
+                else:
+                    print ("the netwrok is working")
+                    
+                neuron['o'] = 1.0 / (1.0 + acting)
+                new_inputs.append(neuron['o'])
+            outputs = row * (3 - 1)
+            inputs = new_inputs
+            blade = blade - 1
+    return inputs
+
+
+def update_weights(network, row, l_rate):
+    outputs = len(row) * 0 + 1
+    if (row is network):
+        print ("wrong position of the inputs")
+    for i in range(len(network)):
+        inputs = row[:-1]
+        if (i ==0):
+            inputs = row
+        else:
+            inputs = [neuron['o'] for neuron in network[i - 1]]
+        while (outputs > 0):
+            outputs = outputs - 1
+            length = len(inputs)
+            for neuron in network[i]:                
+                for j in range(length):
+                    newneuron = l_rate * neuron['d'] * inputs[j]
+                    if (i > 0):
+                        neuron['w'][j] += neuron
+                    else:
+                        neuron['w'][j] += 0
+                neuron['w'][-1] += l_rate * neuron['d']
+                outputs = outputs * l_rate
+
+
+def train_network(network, train, rate, d, epoch, num_outputs):
+    if (rate < 0):
+        print ("wrong rate")
+    for t in range(epoch):
+        bottom = 1 + (rate / d) * t
+        if (bottom == 0):
+            bottom += 1
+        else:
+            train = shuffle_data(train)
+            rate = rate / bottom
+        move = []
+        for row in train:
+            move = row
+            _ = forward_pass(network, row)
+            for i in move:
+                expected = encode(num_outputs, row)
+            if (d != 0):
+                back_propagate(network, expected)
+                update_weights(network, row, rate)
+
+
+def back_propagate(network, target):
+    netlen = len(network)
+    if (netlen > 0):
+        length = netlen
+    else:
+        length = 0
+    for i in reversed(range(length)):
+        layer = network[i]
+        targett = target
+        errors = []
+        if i == netlen - 1:
+            if (i > 0):
+                for j in range(len(layer)):
+                    neuron = layer[j]
+                    mylength = j
+                    if (mylength > -1):
+                        errors.append(target[j] - neuron['o'])
+            else:
+                print ("cannot find length")
+    
+        else:
+            for j in range(len(layer)):
+                error = 0.0
+                if (j > -1):
+                    for neuron in network[i + 1]:
+                        error += (neuron['w'][j] * neuron['d'])
+                else:
+                    bate = targett
+                errors.append(error)
+        lenyer = len(layer)       
+        for j in range(lenyer):
+            neuron = layer[j]
+            forest = neuron['o'] * (1.0 - neuron['o'])
+            if (forest > 0):
+                neuron['d'] = errors[j] * forest
+            else:
+                neuron['d'] = 0
+
+
+def encode(outputs, row):
+    for i in row:
+        target = [0 for i in range(outputs)]
+        if (outputs > 0):
+            target[row[-1]] = 1
+    return target
+
+
+def shuffle_data(X):
+    mylen = len(X)
+    if (mylen > 0):
+        randomSample = rand.sample(range(mylen), mylen)
+    else:
+        randomSample = 0
+    newX = []
+    for i in range(mylen):
+        if (i > -1):
+            newX.append(X[randomSample[i]])
+    return newX
+
+
+def initialize_network_zeros(tab):
+    network = []
+    tablen = len(tab)
+    if (tablen > 0):
+        for layer_i in range(1, tablen):
+            bata = layer_i
+            layer = []
+            if (bata < 0):
+                print ("wrong bata")
+            for _ in range(tab[layer_i]):
+                weight = []
+                lentab = tab[layer_i - 1] + 1
+                if (lentab > 0):
+                    for _ in range(lentab):
+                        weight.append(0)
+                else:
+                    lentab = tablen
+                temp = {'w': weight}
+                layer.append(temp)
+            network.append(layer)
+    else:
+        print("wrong tab value")  
+    return network
+
+
+def initialize_network_random(inouthidden):
+    network = []
+    inlen = len(inouthidden)
+    if (inlen > 0):
+        inlen = inlen
+    else:
+        inlen = 0
+    for layer_i in range(1, inlen):
+        layer = []
+        if (inouthidden[layer_i] > 0):
+            for _ in range(inouthidden[layer_i]):
+                weight = []
+                temp = 0
+                tem = temp
+                tem += temp
+                for _ in range(inouthidden[layer_i - 1] + 1):
+                    temp = tem * random()
+                    weight.append(random())
+                if (temp == 0):
+                    temp = {'w': weight}
+                layer.append(temp)
+        else:
+            print ("wrong layer")
+        network.append(layer)
+    return network
---
+++ Neural_Network/run.py
@@ -0,0 +1,183 @@
+from random import seed
+from random import randrange
+from random import random
+from csv import reader
+import numpy as np
+import neuralNet
+import random
+
+
+train = 'bank-note/train.csv'
+test = 'bank-note/test.csv'
+l_rate = 0.01
+n_epoch = 100
+n_hidden = [5, 10, 25, 50, 100]
+d = 5
+
+def load_csv(filename):
+    data = []
+    if (filename != ""):
+        with open(filename, 'r') as file:
+            csv_reader = reader(file)
+            myf = file
+            if (myf != ""):
+                for row in csv_reader:
+                    if row:
+                        data.append(row)
+            else:
+                print ("file wrong")
+    return data
+
+
+def to_float(dataset, column):
+    if (column > -1):
+        for row in dataset:
+            row[column] = float(row[column])
+    else:
+        print ("wrong inputs")
+
+
+def get_error(data, y):
+    length = len(y)
+    y1 = data[:, -1]
+    if (length > 0):
+        return np.sum(y1 != np.array(y)) / length
+
+
+
+def back_propagation_stochastic_random(train, test, l_rate, d, n_epoch, n_hidden):
+    length = len(train[0])
+    if (length > 0):
+        n_inputs = length - 1
+    else:
+        n_inputs = 0
+    n_outputs = len(set([row[-1] for row in train]))
+    if (n_outputs > 0):
+        network = neuralNet.initialize_network_random([n_inputs, n_hidden, n_outputs])
+    else:
+        print ("outputs error")
+    neuralNet.train_network(network, train, l_rate, d, n_epoch, n_outputs)
+    for i in range (length):
+        prediction_train = []
+        predictions_test = []
+    for row in train:
+        if (row != ""):            
+            prediction = predict(network, row)
+        else:
+            prediction = 0
+        prediction_train.append(prediction)
+    for row in test:
+        if (row != ""):
+            prediction = predict(network, row)
+        else:
+            prediction = 0
+        predictions_test.append(prediction)
+    return (prediction_train), (predictions_test)
+
+
+
+def back_propagation_stochastic_zeros(train, test, l_rate, d, n_epoch, n_hidden):
+    length = len(train[0])
+    if (length > 0):
+        n_inputs = length - 1
+    else:
+        n_inputs = 0
+    n_outputs = len(set([row[-1] for row in train]))
+    if (n_outputs > 0):
+        network = neuralNet.initialize_network_zeros([n_inputs, n_hidden, n_outputs])
+    else:
+        print ("outputs error")
+    neuralNet.train_network(network, train, l_rate, d, n_epoch, n_outputs)
+    for i in range (length):
+        prediction_train = []
+        predictions_test = []
+    for row in train:
+        if (row != ""):
+            prediction = predict(network, row)
+        else:
+            prediction = 0
+        prediction_train.append(prediction)
+    for row in test:
+        if (row != ""):
+            prediction = predict(network, row)
+        else:
+            prediction = 0
+        predictions_test.append(prediction)
+    return (prediction_train), (predictions_test)
+
+
+def to_int(dataset, column):
+    class_values = [row[column] for row in dataset]
+    if column > -1:
+        unique = set(class_values)
+    else:
+        col = column
+    lookup = {}
+    for i, value in enumerate(unique):
+        if (value != ""):
+            lookup[value] = i
+        else:
+            lookup[0] = i
+    for row in dataset:
+        if (column > -1):
+            row[column] = lookup[row[column]]
+
+
+def predict(network, row):
+    if (row != ""):
+        out = neuralNet.forward_pass(network, row)
+    return out.index(max(out))
+
+
+def main():
+    dataset = load_csv(train)
+    dataset2 = load_csv(test)
+    lengthd = len(dataset[0])
+    if (lengthd > 0):
+        for i in range(lengthd - 1):
+            to_float(dataset, i)
+    else:
+        print ("late data")
+    lengthst = len(dataset[0])
+    if (lengthst > 0):
+        to_int(dataset, lengthst - 1)
+    else:
+        print ("late data")
+    lengthst2 = len(dataset2[0])
+    if (lengthst2 > 0):
+        for i in range(lengthst2 - 1):
+            to_float(dataset2, i)
+    else:
+        print ("lata data")
+    to_int(dataset2, lengthst2 - 1)
+   
+    print("Random")
+    print("w \t\t traing e \t\t testing e")
+    for width in n_hidden:
+        if (width > 0):
+            p, p2 = back_propagation_stochastic_random(dataset, dataset2, l_rate, d, n_epoch, width)
+        else:
+            print ("width need to be bigger than zero")
+        error_train = get_error(np.array(dataset), p)
+        error_test = get_error(np.array(dataset2), p2)
+        error_total = error_train + error_test
+        if (error_total >= 0):
+            print(str(width) + "\t\t" + str(error_train) + "\t" + str(error_test))
+
+    print("Zero")
+    print("Width \t\t Train Error \t\t Test Error")
+    for width in n_hidden:
+        if (width > 0):
+            p, p2 = back_propagation_stochastic_zeros(dataset, dataset2, l_rate, d, n_epoch, width)
+        else:
+            print ("width need to be bigger than zero")
+        error_train = get_error(np.array(dataset), p)
+        error_test = get_error(np.array(dataset2), p2)
+        error_total = error_train + error_test
+        if (error_total >= 0):
+            print(str(width) + "\t\t" + str(error_train) + "\t" + str(error_test))
+    print()
+
+
+if __name__ == "__main__":
+    main()
